05fa0a3dbfc7ff549c72f6b50d8162b155a0eb20
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-03-07 18:01:51 +0200

    add Search Bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,28 @@
-export default function SearchBar({ onSubmit }) {
+import iziToast from 'izitoast';
+
+const SearchBar = ({ onSearch }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const searchValue = form.elements.searchInput.value;
+
+    if (searchValue.trim() === '') {
+      iziToast.warning({
+        message: 'Please enter the search term!',
+        position: 'topRight',
+      });
+      return;
+    }
+    onSearch(searchValue);
+    form.reset();
+  };
+
   return (
     <header>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="searchInput"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
@@ -12,4 +31,6 @@ export default function SearchBar({ onSubmit }) {
       </form>
     </header>
   );
-}
+};
+
+export default SearchBar;
