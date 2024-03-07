40b861d25d3e407976c99bf694c0e32eb4087584
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-03-07 18:00:20 +0200

    add API

---
+++ src/articles-api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_URL = 'https://api.unsplash.com';
+const ACCESS_KEY = 'IMgd-JZNb9BN6sDOuDvJqXDAPY6O_etmim3DLFJMLNM';
+
+axios.defaults.baseURL = API_URL;
+
+const searchParms = {
+  client_id: ACCESS_KEY,
+  query: '',
+  page: 1,
+  per_page: 18,
+  orientation: 'landscape',
+};
+
+export const handleFetchPhotos = async (query, page) => {
+  const res = await axios.get('/search/photos', {
+    params: {
+      ...searchParms,
+      page,
+      query,
+    },
+  });
+  return res.data;
+};
